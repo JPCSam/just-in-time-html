e9e0a58b08d5aa440f1db579113db2c6d459b2c3
Sam <sam@jpcsocial.co.uk>
2023-01-25 15:48:32 +0000

    Continued work on new layout and design.

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,56 @@ ScrollTrigger.create({
 		targets: '.navbar',
 	},
 });
+
+gsap.defaults({ overwrite: 'auto' });
+
+// Set up our scroll trigger
+const services_trigger = ScrollTrigger.create({
+	trigger: '#services-section',
+	start: 'top top',
+	end: 'bottom 100%',
+	onUpdate: getCurrentSection,
+	pin: '.left-content',
+});
+
+//Services sections fade-in effect
+gsap.utils.toArray('.services-card').forEach((section, i) => {
+	gsap.to(section.querySelector('.heading'), {
+		opacity: 1,
+		scrollTrigger: {
+			trigger: '.services-card',
+			pinnedContainer: 'main',
+			pin: 'main',
+			start: 'center center',
+			end: '+=300',
+			toggleActions: 'play none none reverse',
+		},
+	});
+});
+const contentMarkers = gsap.utils.toArray('.contentMarker');
+
+// Handle the updated position
+let lastContent;
+function getCurrentSection() {
+	let newContent;
+	const currScroll = scrollY;
+
+	// Find the current section
+	contentMarkers.forEach((marker) => {
+		if (currScroll > marker.offsetTop) {
+			newContent = marker.content;
+		}
+	});
+}
+
+const media = window.matchMedia('screen and (max-width: 667px)');
+ScrollTrigger.addEventListener('refreshInit', checkSTState);
+checkSTState();
+
+function checkSTState() {
+	if (media.matches) {
+		services_trigger.disable();
+	} else {
+		services_trigger.enable();
+	}
+}
